ddb949a7edb58a7e180e4beb9ba677eec0bba699
BrianC <brian@debian-BULLSEYE-live-builder-AMD64>
2023-04-05 10:16:29 -0500

    find name method fixed 1

--- myApp.js
+++ myApp.js
@@ -34,7 +34,7 @@ const createManyPeople = (arrayOfPeople, done) => {
 };
 
 const findPeopleByName = (personName, done) => {
-  Person.find(personName, function(err, data) {
+  Person.find({name: personName}, function(err, data) {
     if (err) return console.error(err);
     done(null, data)
   });
