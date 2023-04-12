5e95419e3313a4329c6ce8e50d09743b361ce12a
Brian <brian@brianpc>
2023-04-11 22:51:55 -0500

    find various documents with find method

--- myApp.js
+++ myApp.js
@@ -118,9 +118,16 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  Person.find({favoriteFoods: foodToSearch}).sort({name: "asc"}).limit(2).select("-age").exec(function(err, data) {
+    if (err) return console.error(err);
+    // console.log("Se encontraron los sgtes registros: ", data)
+    done(null, data)
+  })
 };
+
+/** Test */
+// queryChain()
+/** End test */
 
 /** **Well Done !!**
 /* You completed these challenges, let's go celebrate !
